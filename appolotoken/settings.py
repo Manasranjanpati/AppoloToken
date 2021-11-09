a0a6f9485807b6d4d5824a0dce1c9fbd6e45c6de
Manas Ranjan Pati <iammanas24@gmail.com>
2021-11-09 12:01:06 +0530

    user module

--- appolotoken/settings.py
+++ appolotoken/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tickets',
+    'users',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# My settings
+LOGIN_URL = 'users:login'
